5bb90fb109f87a671a77d476325ba70cf89723fc
stnava <stnava@gmail.com>
2024-02-04 11:38:02 -0500

    WIP: citations

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,7 @@
 cdir=`pwd`
+
 cd /Users/stnava/Documents/writing/ppmi_mri_scidata/src
+# sed -i.bak 's/[\d128-\d255]//g' src/ppmiscidata.bib
+# mv ppmiscidata.bib.bak ppmiscidata2.bib
 Rscript -e 'rmarkdown::render("ppmi_sci_data.Rmd")'
 cd $cdir
