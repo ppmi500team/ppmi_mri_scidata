330871d116c7b1e0969a8a3febf881a034c056a9
stnava <stnava@gmail.com>
2024-02-08 10:49:53 -0500

    ENH: build script

--- src/build.sh
+++ src/build.sh
@@ -3,5 +3,8 @@ cdir=`pwd`
 cd /Users/stnava/Documents/writing/ppmi_mri_scidata/src
 # sed -i.bak 's/[\d128-\d255]//g' src/ppmiscidata.bib
 # mv ppmiscidata.bib.bak ppmiscidata2.bib
-Rscript -e 'rmarkdown::render("ppmi_sci_data.Rmd")'
+# "OR","SRF","SRP"
+Rscript -e 'myrez="OR"; ofn=paste0("ppmi_sci_data_",myrez,".pdf"); rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
+Rscript -e 'myrez="SRF"; ofn=paste0("ppmi_sci_data_",myrez,".pdf"); rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
+Rscript -e 'myrez="SRP"; ofn=paste0("ppmi_sci_data_",myrez,".pdf"); rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
 cd $cdir
