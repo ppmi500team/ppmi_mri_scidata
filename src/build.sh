77a651317b07ce388585a6d2f67df1a62c13da86
stnava <stnava@gmail.com>
2024-03-10 14:22:38 -0400

    WIP: ...

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@ cd /Users/stnava/Documents/writing/ppmi_mri_scidata/src
 # sed -i.bak 's/[\d128-\d255]//g' src/ppmiscidata.bib
 # mv ppmiscidata.bib.bak ppmiscidata2.bib
 # "OR","SRF","SRP"
-Rscript -e 'myrez="OR"; ofn=paste0("ppmi_sci_data_",myrez,".pdf"); if ( ! file.exists(ofn) ) rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
-Rscript -e 'myrez="SRF"; ofn=paste0("ppmi_sci_data_",myrez,".pdf");  if ( ! file.exists(ofn) )rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
+Rscript -e 'myrez="OR"; ofn=paste0("ppmi_sci_data_",myrez,".pdf"); if ( ! file.exists(ofn) ) rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn, clean=FALSE )'
+Rscript -e 'myrez="SRF"; ofn=paste0("ppmi_sci_data_",myrez,".pdf");  if ( ! file.exists(ofn) )rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn, clean=FALSE)'
 Rscript -e 'myrez="SRP"; ofn=paste0("ppmi_sci_data_",myrez,".pdf");  if ( ! file.exists(ofn) ) rmarkdown::render("ppmi_sci_data.Rmd", output_file=ofn )'
 cd $cdir
